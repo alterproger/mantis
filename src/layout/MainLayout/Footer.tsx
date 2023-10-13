3718a408eb00d8fdcffe42b1835a346f2285962c
alterproger <alterproger@gmail.com>
2023-10-13 14:09:27 +0300

    Fix responsive design in the Footer

--- src/layout/MainLayout/Footer.tsx
+++ src/layout/MainLayout/Footer.tsx
@@ -1,8 +1,10 @@
 import Stack from '@mui/material/Stack';
 import { Link as RouterLink } from 'react-router-dom';
-import { Typography, Link, Box } from '@mui/material';
+import { Typography, Link, Box, useMediaQuery } from '@mui/material';
 
 import { FacebookOutlined, InstagramOutlined, TwitterOutlined } from '@ant-design/icons';
+
+import { Theme } from '@mui/material/styles';
 
 const linkColumns = [
   {
@@ -23,123 +25,127 @@ const linkColumns = [
   }
 ];
 
-const Footer = () => (
-  <Stack
-    direction="column"
-    sx={{
-      p: '48px 48px 10px 48px',
-      mt: 'auto',
-      backgroundColor: '#262626'
-    }}
-  >
-    <Box display="flex" justifyContent="space-between" marginBottom="64px" flexWrap="wrap" gap="40px">
-      {linkColumns.map((column, index) => (
-        <Box key={index}>
-          <Typography
-            variant="h6"
+const Footer = () => {
+  const matchDownSM = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+
+  return (
+    <Stack
+      direction="column"
+      sx={{
+        p: `${matchDownSM ? '22px 22px 10px 22px' : '48px 48px 10px 48px'}`,
+        mt: 'auto',
+        backgroundColor: '#262626'
+      }}
+    >
+      <Box display="flex" justifyContent="space-between" marginBottom="64px" flexWrap="wrap" gap="40px">
+        {linkColumns.map((column, index) => (
+          <Box key={index}>
+            <Typography
+              variant="h6"
+              sx={{
+                width: '223px',
+                fontSize: 16,
+                fontWeight: 500,
+                lineHeight: '150%',
+                marginBottom: `${matchDownSM ? '30px' : '56px'}`,
+                color: 'white'
+              }}
+            >
+              {column.heading}
+            </Typography>
+            {column.links?.map((link, linkIndex) => (
+              <Link
+                component={RouterLink}
+                to="/"
+                variant="body1"
+                color="#BFBFBF"
+                sx={{
+                  display: 'block',
+                  padding: '12px 24px 12px 0',
+                  fontSize: 14,
+                  lineHeight: '157%'
+                }}
+                key={linkIndex}
+              >
+                {link}
+              </Link>
+            ))}
+            {column.text && (
+              <Typography
+                variant="body1"
+                sx={{
+                  display: 'block',
+                  fontSize: 14,
+                  lineHeight: '157%',
+                  color: '#BFBFBF',
+                  maxWidth: '411px'
+                }}
+              >
+                {column.text}
+              </Typography>
+            )}
+          </Box>
+        ))}
+      </Box>
+
+      <Box display="flex" justifyContent="space-between" alignItems="center" flexWrap="wrap" gap="30px">
+        <Typography variant="caption" sx={{ color: '#BFBFBF' }}>
+          &copy; 2023 Artboard. All rights reserved. Legal information
+        </Typography>
+
+        <Box display="flex" alignItems="center" gap="40px" marginRight="48px">
+          <Link
+            variant="caption"
+            component={RouterLink}
+            to="/"
+            target="_blank"
             sx={{
-              width: '223px',
-              fontSize: 16,
-              fontWeight: 500,
-              lineHeight: '150%',
-              marginBottom: '56px',
-              color: 'white'
+              fontSize: '19px',
+              color: '#FAFAFA',
+              transition: 'color 0.3s',
+              '&:hover': {
+                color: '#1890FF'
+              }
             }}
           >
-            {column.heading}
-          </Typography>
-          {column.links?.map((link, linkIndex) => (
-            <Link
-              component={RouterLink}
-              to="/"
-              variant="body1"
-              color="#BFBFBF"
-              sx={{
-                display: 'block',
-                padding: '12px 24px 12px 0',
-                fontSize: 14,
-                lineHeight: '157%'
-              }}
-              key={linkIndex}
-            >
-              {link}
-            </Link>
-          ))}
-          {column.text && (
-            <Typography
-              variant="body1"
-              sx={{
-                display: 'block',
-                fontSize: 14,
-                lineHeight: '157%',
-                color: '#BFBFBF',
-                maxWidth: '411px'
-              }}
-            >
-              {column.text}
-            </Typography>
-          )}
+            <FacebookOutlined />
+          </Link>
+          <Link
+            variant="caption"
+            component={RouterLink}
+            to="/"
+            target="_blank"
+            sx={{
+              fontSize: '19px',
+              color: '#FAFAFA',
+              transition: 'color 0.3s',
+              '&:hover': {
+                color: '#1890FF'
+              }
+            }}
+          >
+            <InstagramOutlined />
+          </Link>
+          <Link
+            variant="caption"
+            component={RouterLink}
+            to="/"
+            target="_blank"
+            sx={{
+              fontSize: '19px',
+              color: '#FAFAFA',
+              transition: 'color 0.3s',
+              '&:hover': {
+                color: '#1890FF'
+              }
+            }}
+          >
+            <TwitterOutlined />
+          </Link>
         </Box>
-      ))}
-    </Box>
-
-    <Box display="flex" justifyContent="space-between" alignItems="center" flexWrap="wrap">
-      <Typography variant="caption" sx={{ color: '#BFBFBF' }}>
-        &copy; 2023 Artboard. All rights reserved. Legal information
-      </Typography>
-
-      <Box display="flex" alignItems="center" gap="40px" marginRight="48px">
-        <Link
-          variant="caption"
-          component={RouterLink}
-          to="/"
-          target="_blank"
-          sx={{
-            fontSize: '19px',
-            color: '#FAFAFA',
-            transition: 'color 0.3s',
-            '&:hover': {
-              color: '#1890FF'
-            }
-          }}
-        >
-          <FacebookOutlined />
-        </Link>
-        <Link
-          variant="caption"
-          component={RouterLink}
-          to="/"
-          target="_blank"
-          sx={{
-            fontSize: '19px',
-            color: '#FAFAFA',
-            transition: 'color 0.3s',
-            '&:hover': {
-              color: '#1890FF'
-            }
-          }}
-        >
-          <InstagramOutlined />
-        </Link>
-        <Link
-          variant="caption"
-          component={RouterLink}
-          to="/"
-          target="_blank"
-          sx={{
-            fontSize: '19px',
-            color: '#FAFAFA',
-            transition: 'color 0.3s',
-            '&:hover': {
-              color: '#1890FF'
-            }
-          }}
-        >
-          <TwitterOutlined />
-        </Link>
       </Box>
-    </Box>
-  </Stack>
-);
+    </Stack>
+  );
+};
 
 export default Footer;
