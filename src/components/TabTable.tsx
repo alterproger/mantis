8af1e5f53df42de64fae08f98bbdcd649d8b3c21
alterproger <alterproger@gmail.com>
2023-10-13 12:53:55 +0300

    Update tab table styles

--- src/components/TabTable.tsx
+++ src/components/TabTable.tsx
@@ -7,7 +7,7 @@ import {
   InfoCircleOutlined
 } from '@ant-design/icons';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
-import { Box, CardContent, Grid, IconButton, Link, Tab, TableContainer, Typography } from '@mui/material';
+import { Box, CardContent, Chip, Grid, IconButton, Link, Tab, TableContainer, Typography } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -47,7 +47,7 @@ const completedList = [
 ];
 
 const CustomTableCell = ({ title, children }: { title: string; children: ReactNode }) => (
-  <TableCell sx={{}}>
+  <TableCell>
     <Grid container sx={{}}>
       <Grid item xs={12}>
         <Typography sx={{ color: '#8C8C8C', fontSize: '12px' }}>{title}</Typography>
@@ -60,13 +60,11 @@ const CustomTableCell = ({ title, children }: { title: string; children: ReactNo
   </TableCell>
 );
 
-const TabLabel = ({ name, amount }: { name: string; amount: number }) => {
+const TabLabel = ({ name, amount, color }: { name: string; amount: number; color: 'info' | 'success' | 'warning' }) => {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'start', alignItems: 'center', gap: '4px' }}>
       <Typography>{name}</Typography>
-      <Typography sx={{ background: '#E6F7FF', color: '#1890FF', display: 'block', padding: '0 6px', borderRadius: '100%' }}>
-        {amount}
-      </Typography>
+      <Chip variant="combined" color={color} label={amount} sx={{ border: 'none', borderRadius: '100%' }} size="small" />
     </Box>
   );
 };
@@ -84,11 +82,25 @@ const CustomTable = ({
   badge: ReactNode;
 }) => (
   <TableContainer component={Paper} sx={{ boxShadow: 'none' }}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <Table sx={{ minWidth: 650 }}>
       <TableBody>
         {rows.map((row) => (
-          <TableRow key={row.name} sx={{ background: '#FAFAFA', borderTop: '12px solid white' }}>
-            <TableCell>{badge}</TableCell>
+          <TableRow
+            key={row.name}
+            sx={{
+              background: '#FAFAFA',
+              borderTop: '12px solid white'
+            }}
+          >
+            <TableCell
+              sx={{
+                '&:first-of-type': {
+                  paddingLeft: '12px !important'
+                }
+              }}
+            >
+              {badge}
+            </TableCell>
             <CustomTableCell title="Task Name">{row.task}</CustomTableCell>
             <CustomTableCell title="Assigned">
               <Box sx={{ display: 'flex', justifyContent: 'start', gap: '6px', alignItems: 'center' }}>
@@ -112,7 +124,13 @@ const CustomTable = ({
                 <Typography>{row.date}</Typography>
               </Box>
             </CustomTableCell>
-            <TableCell sx={{}}>
+            <TableCell
+              sx={{
+                '&:last-of-type': {
+                  paddingRight: '0 !important'
+                }
+              }}
+            >
               <IconButton color="secondary" sx={{ transform: 'rotate(90deg)' }}>
                 <EllipsisOutlined />
               </IconButton>
@@ -143,10 +161,10 @@ export default function TabTable() {
       <CardContent>
         <TabContext value={tab}>
           <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label={<TabLabel name="Active" amount={activeList.length} />} value="active" />
-              <Tab label={<TabLabel name="Pending" amount={pendingList.length} />} value="pending" />
-              <Tab label={<TabLabel name="Completed" amount={completedList.length} />} value="completed" />
+            <TabList onChange={handleChange}>
+              <Tab label={<TabLabel color="info" name="Active" amount={activeList.length} />} value="active" />
+              <Tab label={<TabLabel color="warning" name="Pending" amount={pendingList.length} />} value="pending" />
+              <Tab label={<TabLabel color="success" name="Completed" amount={completedList.length} />} value="completed" />
             </TabList>
             <Box sx={{ fontSize: '20px', p: '0 18px', color: '#8C8C8C' }}>
               <FilterOutlined />
